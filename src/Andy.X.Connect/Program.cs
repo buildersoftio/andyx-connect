f0f9a0452fbaf1e2a3323131667b5401327d194e
Enes Hoxha <eneshoxha@outlook.com>
2021-09-05 20:51:19 +0200

    Added info for Andy X Connect

--- src/Andy.X.Connect/Program.cs
+++ src/Andy.X.Connect/Program.cs
@@ -10,9 +10,10 @@ namespace Andy.X.Connect
         {
             GlobalService globalService;
             Console.WriteLine("Buildersoft Andy X Connect");
+            Console.WriteLine("Andy X Connect is an open source distributed platform for change data capture. Start it up, point it at your databases, and your apps can start responding to all of the inserts, updates, and deletes that other apps commit to your databases\n");
 
             globalService = new GlobalService();
-            Logger.LogInformation("services are ready");
+            Logger.LogInformation("Andy X Connect is ready");
 
 
             while (true)
