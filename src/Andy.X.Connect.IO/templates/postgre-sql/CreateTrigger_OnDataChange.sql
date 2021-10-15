5cdac941826fe717f664c1f1ea17ada2a80bdb4c
Enes Hoxha <eneshoxha@outlook.com>
2021-10-15 14:20:39 +0200

    add: add sql code templates for postgre

---
+++ src/Andy.X.Connect.IO/templates/postgre-sql/CreateTrigger_OnDataChange.sql
@@ -0,0 +1,9 @@
+﻿-- Buildersoft
+-- -- Andy X Conenct
+-- -- # Template to create trigger onDataChanged for a table
+
+CREATE TRIGGER "AndyX_OnDataChange"
+  AFTER INSERT OR DELETE OR UPDATE 
+  ON {database_name}.{table_name}
+  FOR EACH ROW
+  EXECUTE PROCEDURE public."AndyX_{table_name}_NotifyOnDataChange"();
